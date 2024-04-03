99bc9c95513d584acea1daecd58e1af2f5c988f4
DadosCoelho <daldocoelho@gmail.com>
2024-04-03 11:19:41 -0300

    Alteração nas msg

--- Milion_Apps.js
+++ Milion_Apps.js
@@ -9,7 +9,7 @@ function importDataF() {
     var Pergunta1 = spreadsheet.getRange('A1').getValue();
     var range1 = spreadsheet.getRange("A2:A1000");
     var choices1 = range1.getValues().map(function(row) {return row[0]});
-    // Remover opções duplicadas
+    //DadosCoelho' Remover opções duplicadas.
     var uniqueChoices1 = [];
     choices1.forEach(function(choice) {
       if (uniqueChoices1.indexOf(choice) === -1) {
@@ -20,10 +20,10 @@ function importDataF() {
       return item.getTitle() === Pergunta1;
     });
     if (existingQuestion1.length > 0) {
-      // Se a pergunta já existir, atualize-a
+      //DadosCoelho' Se a pergunta já existir, atualize-a.
       existingQuestion1[0].asListItem().setChoiceValues(uniqueChoices1);
     } else {
-      // Caso contrário, adicione uma nova pergunta
+      //DadosCoelho' Caso contrário, adicione uma nova pergunta.
       var question1 = form.addListItem();
       question1.setTitle(Pergunta1);
       question1.setChoiceValues(uniqueChoices1);
@@ -34,7 +34,7 @@ function importDataF() {
     var Pergunta2 = spreadsheet.getRange('B1').getValue();
     var range2 = spreadsheet.getRange("B2:B1000");
     var choices2 = range2.getValues().map(function(row) {return row[0]});
-    // Remover opções duplicadas
+    //DadosCoelho' Remover opções duplicadas.
     var uniqueChoices2 = [];
     choices2.forEach(function(choice) {
       if (uniqueChoices2.indexOf(choice) === -1) {
@@ -45,22 +45,22 @@ function importDataF() {
       return item.getTitle() === Pergunta2;
     });
     if (existingQuestion2.length > 0) {
-      // Se a pergunta já existir, atualize-a
+      //DadosCoelho' Se a pergunta já existir, atualize-a.
       existingQuestion2[0].asListItem().setChoiceValues(uniqueChoices2);
     } else {
-      // Caso contrário, adicione uma nova pergunta
+      //DadosCoelho' Caso contrário, adicione uma nova pergunta.
       var question2 = form.addListItem();
       question2.setTitle(Pergunta2);
     }
   
   
-        // Esse grupo de comandos é para excluir perguntas diferentes das perguntas que quero que apareça. 
+        //DadosCoelho' Esse grupo de comandos é para excluir perguntas diferentes das perguntas que quero que apareça. 
     var items = form.getItems();
     for (var i = 0; i < items.length; i++) {
       var item = items[i];
       var title = item.getTitle();
       if (title !== Pergunta1 && title !== Pergunta2) {
-        // Se o título do item for diferente do título fornecido, remova-o do formulário.
+        //DadosCoelho' Se o título do item for diferente do título fornecido, remova-o do formulário.
         form.deleteItem(item);
       }
     }
