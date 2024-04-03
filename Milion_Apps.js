61e25c6bde91f9e38b57dff0fbe9369cf6168443
contatop <ricardo.adm115@gmail.com>
2024-04-03 11:44:33 -0300

    adição a pergunta 3

--- Milion_Apps.js
+++ Milion_Apps.js
@@ -55,26 +55,26 @@ function importDataF() {
   
 
     // Essa é a pergunta 3
-    var Pergunta2 = spreadsheet.getRange('B1').getValue();
-    var range2 = spreadsheet.getRange("B2:B1000");
-    var choices2 = range2.getValues().map(function(row) {return row[0]});
+    var Pergunta3 = spreadsheet.getRange('C1').getValue();
+    var range3 = spreadsheet.getRange("C2:C1000");
+    var choices3 = range3.getValues().map(function(row) {return row[0]});
     // Remover opções duplicadas
-    var uniqueChoices2 = [];
-    choices2.forEach(function(choice) {
-      if (uniqueChoices2.indexOf(choice) === -1) {
-        uniqueChoices2.push(choice);
+    var uniqueChoices3 = [];
+    choices3.forEach(function(choice) {
+      if (uniqueChoices3.indexOf(choice) === -1) {
+        uniqueChoices3.push(choice);
       }
     });
-    var existingQuestion2 = form.getItems(FormApp.ItemType.LIST).filter(function(item) {
+    var existingQuestion3 = form.getItems(FormApp.ItemType.LIST).filter(function(item) {
       return item.getTitle() === Pergunta3;
     });
-    if (existingQuestion2.length > 0) {
+    if (existingQuestion3.length > 0) {
       // Se a pergunta já existir, atualize-a
-      existingQuestion2[0].asListItem().setChoiceValues(uniqueChoices2);
+      existingQuestion3[0].asListItem().setChoiceValues(uniqueChoices3);
     } else {
       // Caso contrário, adicione uma nova pergunta
-      var question2 = form.addListItem();
-      question2.setTitle(Pergunta3);
+      var question3 = form.addListItem();
+      question3.setTitle(Pergunta3);
     }
 
   
